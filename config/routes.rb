ecbf2b998a50ac9e9b470c979e421e1fa5827924
Valentyne <dr_th@ukr.net>
2024-08-19 17:34:05 +0300

    Add comments to post

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   #get 'contacts' => 'contacts#new'
   #resource :contacts, only: [:create]
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
-  resources :articles
+  resources :articles do
+  	resources :comments
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
